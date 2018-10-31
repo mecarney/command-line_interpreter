6031d51df09946663cf24f1c1995c38156653d50
maxencejded <maxence.jded@gmail.com>
2018-10-31 16:23:02 -0700

    Handle multiple $ for string

--- srcs/ft_expand.c
+++ srcs/ft_expand.c
@@ -6,7 +6,7 @@
 /*   By: mjacques <mjacques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/30 23:17:33 by mjacques          #+#    #+#             */
-/*   Updated: 2018/10/30 23:21:51 by mjacques         ###   ########.fr       */
+/*   Updated: 2018/10/31 16:21:30 by mjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,18 @@ static char	*ft_expand(char *str, char *name, int pos)
 	envar = ft_envar(name);
 	len = ft_strlen(name) + 1;
 	if (envar == -1)
-		tmp = ft_strjoin("", &str[pos]);
+		tmp = ft_strdup(&str[pos]);
 	else
 		tmp = ft_strjoin(&g_envp[envar][len], &str[pos]);
 	return (tmp);
 }
 
-static char	*ft_check_tilde(char *cmd, char *tilde)
+static char	*ft_check_tilde(char *cmd)
 {
-	int		pos;
-
-	pos = tilde - cmd + 1;
-	if (!cmd[pos])
-		cmd = free_str(cmd, ft_expand(cmd, "HOME", pos));
-	else if (cmd[pos] && (cmd[pos] == '/'))
-		cmd = free_str(cmd, ft_expand(cmd, "HOME", pos));
+	if (!cmd[1])
+		cmd = free_str(cmd, ft_expand(cmd, "HOME", 1));
+	else if (cmd[1] && (cmd[1] == '/'))
+		cmd = free_str(cmd, ft_expand(cmd, "HOME", 1));
 	return (cmd);
 }
 
@@ -48,7 +45,7 @@ static char	*ft_variable_len(char *cmd, int *pos)
 	while (cmd[*pos + len] && cmd[*pos + len] > 47)
 		len++;
 	name = ft_strmcpy(&cmd[*pos], len);
-	*pos = len;
+	*pos += len - 1;
 	return (name);
 }
 
@@ -78,10 +75,10 @@ char		**ft_check_expand(char **cmd)
 	i = 0;
 	while (cmd[++i])
 	{
-		if ((position = ft_strchr(cmd[i], '$')))
+		if (cmd[i][0] && cmd[i][0] == '~')
+			cmd[i] = ft_check_tilde(cmd[i]);
+		while ((position = ft_strchr(cmd[i], '$')))
 			cmd[i] = ft_check_dollar(cmd[i], position);
-		if ((position = ft_strchr(cmd[i], '~')))
-			cmd[i] = ft_check_tilde(cmd[i], position);
 	}
 	return (cmd);
 }
