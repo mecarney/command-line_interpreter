e9d4ed3344f5c707a3ac1d4228ffb05dbba8aa03
maxencejded <maxence.jded@gmail.com>
2018-11-10 13:45:51 -0800

    Fix usless fork()

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mjacques <mjacques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 14:15:18 by mjacques          #+#    #+#             */
-/*   Updated: 2018/11/10 12:47:54 by mcarney          ###   ########.fr       */
+/*   Updated: 2018/11/10 13:18:06 by mjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,49 +24,44 @@ t_builtin	g_builtin[NBRBUILTIN] = {
 	{"history", &ft_builtin_history}
 };
 
-void	input_loop()
+void	ft_init_struct(t_info *t)
+{
+	t->i = -1;
+	t->j = 0;
+	t->expand = 0;
+	t->operator = 0;
+	t->prev = '\0';
+	t->tokens = NULL;
+}
+
+void	input_loop(_Bool ret)
 {
 	char	*line;
-	_Bool	ret;
-	pid_t	child;
-	int		status;
 
-	if ((child = fork()) == 0)
+	while (42)
 	{
-		ret = 0;
-		while (42)
+		(ret) ? ft_putstr(RED) : ft_putstr(PURPLE);
+		ft_putstr(SHELLNAME);
+		if (get_next_line(0, &line) > 0)
 		{
-			(ret) ? ft_putstr(RED) : ft_putstr(PURPLE);
-			ft_putstr(SHELLNAME);
-			if (get_next_line(0, &line) > 0)
-			{
-				ft_run_shell(&ret, line);
-				ft_strdel(&line);
-			}
+			ft_run_shell(&ret, line);
+			ft_strdel(&line);
 		}
 	}
-	else if (child < 0)
-		ft_error("bls-bla-bal");
-	waitpid(child, &status, WUNTRACED);
 }
 
 void	ft_restart(t_ast *tokens, char *msg)
 {
 	ft_putendl(msg);
 	free_ast(tokens);
-	input_loop();
+	input_loop(1);
 }
 
 void	ft_run_shell(_Bool *ret, char *line)
 {
 	t_info	t;
 
-	t.i = -1;
-	t.j = 0;
-	t.expand = 0;
-	t.operator = 0;
-	t.prev = '\0';
-	t.tokens = NULL;
+	ft_init_struct(&t);
 	check_quotes(line, &t, &t.tokens);
 	t.tokens = parser(&t.tokens, NULL);
 	*ret = ft_tokens_exec(t.tokens);
@@ -75,26 +70,14 @@ void	ft_run_shell(_Bool *ret, char *line)
 
 int		main(int argc, char **argv, char **envp)
 {
-	// char	*line;
-	// _Bool	ret;
+	_Bool	ret;
 
 	(argc != 1 || !argv[0]) ? ft_error("Usage: ./42sh") : NULL;
 	ft_printf("%s%sWelcome!%s Make great code today\n", BOLD, BLUE, REGULAR);
 	envp = ft_newenv(envp);
 	g_history = NULL;
 	g_history_file = get_history_file();
-
-	input_loop();
-	// ret = 0;
-	// while (42)
-	// {
-	// 	(ret) ? ft_putstr(RED) : ft_putstr(PURPLE);
-	// 	ft_putstr(SHELLNAME);
-	// 	if (get_next_line(0, &line) > 0)
-	// 	{
-	// 		ft_run_shell(&ret, line);
-	// 		ft_strdel(&line);
-	// 	}
-	// }
+	ret = 0;
+	input_loop(ret);
 	return (0);
 }
