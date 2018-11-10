9ff1fa35484ed5a15d201af6e2f94f692ecb410d
maxencejded <maxence.jded@gmail.com>
2018-11-10 14:34:41 -0800

    Fixing some errors for the redirection

--- srcs/ft_tokens_fct.c
+++ srcs/ft_tokens_fct.c
@@ -6,7 +6,7 @@
 /*   By: mjacques <mjacques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 13:01:24 by mjacques          #+#    #+#             */
-/*   Updated: 2018/11/04 18:26:46 by mjacques         ###   ########.fr       */
+/*   Updated: 2018/11/10 14:25:22 by mjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ void		ft_tokens_redirect(t_ast *tokens, _Bool *ret)
 	int			fd_new;
 	int			fd_base;
 	char		*file;
-	static char	*cat[] = {"cat", 0};
 
 	fd_new = 1;
 	fd_base = dup(1);
@@ -42,8 +41,6 @@ void		ft_tokens_redirect(t_ast *tokens, _Bool *ret)
 		dup2(fd_new, 1);
 		if (tokens->l_child)
 			*ret = ft_tokens_exec(tokens->l_child);
-		else
-			*ret = ft_run_cmd(cat);
 		close(fd_new);
 		dup2(fd_base, 1);
 	}
@@ -54,7 +51,6 @@ void		ft_tokens_redirect_append(t_ast *tokens, _Bool *ret)
 	int			fd_new;
 	int			fd_base;
 	char		*file;
-	static char	*cat[] = {"cat", 0};
 
 	fd_new = 1;
 	fd_base = dup(1);
@@ -68,8 +64,6 @@ void		ft_tokens_redirect_append(t_ast *tokens, _Bool *ret)
 		lseek(fd_new, 0, SEEK_END);
 		if (tokens->l_child)
 			*ret = ft_tokens_exec(tokens->l_child);
-		else
-			*ret = ft_run_cmd(cat);
 		close(fd_new);
 		dup2(fd_base, 1);
 	}
@@ -84,22 +78,17 @@ void		ft_tokens_redirect_fd(t_ast *tokens, _Bool *ret)
 
 	if ((fd_new = open(ft_tokens_val(tokens->r_child), O_RDONLY)) == -1)
 		*ret = ft_return_access("42sh", ft_tokens_val(tokens->r_child));
-	else
+	else if (tokens->l_child)
 	{
-		if (!tokens->l_child)
-			*ret = ft_tokens_file(fd_new);
-		else
-		{
-			std_out = dup(0);
-			dup2(fd_new, 0);
-			if ((child = fork()) == 0)
-				exit(ft_tokens_exec(tokens->l_child));
-			else if (child < 0)
-				ft_putendl("ERROR: fork() failed");
-			close(fd_new);
-			waitpid(child, &stat_loc, WUNTRACED);
-			dup2(std_out, 0);
-			*ret = stat_loc;
-		}
+		std_out = dup(0);
+		dup2(fd_new, 0);
+		if ((child = fork()) == 0)
+			exit(ft_tokens_exec(tokens->l_child));
+		else if (child < 0)
+			ft_putendl("ERROR: fork() failed");
+		close(fd_new);
+		waitpid(child, &stat_loc, WUNTRACED);
+		dup2(std_out, 0);
+		*ret = stat_loc;
 	}
 }
